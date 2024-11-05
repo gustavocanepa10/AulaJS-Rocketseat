b12c42274a07efe3a4c3258bde7966ab3f437af9
João Gustavo <gustavociscotto@gmail.com>
2024-11-05 10:21:02 -0300

    continuando estudando repetição

--- Repetição e iteração/aula6.js
+++ Repetição e iteração/aula6.js
@@ -13,11 +13,35 @@ let person = {
 
 
 
+
+// Percorrendo as propriedades a partir de um objeto
 for (let property in person) {
 
+    
+    
 
-
-    // Exibe o nome da propriedade
+    // Exibe o nome da propriedade.
     console.log(property)
+
+    
+    // Exibe o conteúdo da propriedade
+    console.log(person[property])
+    
+
+    
+
+
+}
+
+let students = ["Gustavo", "Manuela", "Amanda"]
+
+
+// A cada iteração do loop, a variavel index recebe o indice atual do array (0, 1 , 2 )
+for (let index in students) {
+    // console.log(index)
+
+    // Aqui eu imprimo o valor do elemento localizado no indice atual.
+    console.log(students[index])
+    // O index é usado para acessar o valor de cada do array students, então students[index] 
 
 }
---
+++ Repetição e iteração/aula7.js
@@ -0,0 +1,20 @@
+// for....of itera sobre valores de um objeto iterável.
+
+
+let students = ["João", "Amanda", "Pedro"]
+
+for (let student of students) {
+
+  console.log(student)
+
+
+}
+
+
+let user = {
+  name:"Rodrigo",
+
+}
+  
+
+
