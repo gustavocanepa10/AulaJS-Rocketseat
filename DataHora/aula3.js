9ea172cf96e77b9afaf936e09b41904d2a651dad
Gustavo Canepae <gustavociscotto@gmail.com>
2024-11-06 10:42:52 -0300

    Avançando nos estudos de data e hora

---
+++ DataHora/aula3.js
@@ -0,0 +1,39 @@
+// Métodos para trabalhar com data e hora.
+
+let date = new Date("2024-07-02T14:30:10")
+
+
+// Dia da semana de 0 à 6 (domingo é 0)
+console.log(date.getDay())
+
+/* 0 - Domingo
+   1 - Segunda
+   2 - Terça
+   3 - Quarta
+   4 - Quinta
+   5 - Sexta
+   6 - Sábado
+*/
+
+
+// Dia do mês (0 à 30)
+console.log(date.getDate())
+
+
+// Mês (0 à 11)
+console.log(date.getMonth() + 1)
+
+
+// Ano
+console.log(date.getFullYear())
+
+// Horas
+console.log(date.getHours())
+
+
+// Minutos
+console.log(date.getMinutes())
+
+// Segundos
+console.log(date.getSeconds())
+
