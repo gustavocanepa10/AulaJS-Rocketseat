12d4504f716d55afd8d4c3a2fc3a93b4ea849e73
João Gustavo <gustavociscotto@gmail.com>
2024-12-10 09:42:44 -0300

    Começando módulos

---
+++ módulos/ESmodules.js
@@ -0,0 +1,15 @@
+/* É o sistema de módulo Javascript.
+Define um formato para organizar e estruturar o código
+em módulos, permitindo a modularização e reutilização
+de código.*/
+
+/* Módulos: encapsulamento de código em módulos,
+permitindo a separação de responsabilidades e 
+reutilização de código.
+
+
+import: usado para incluir módulos.
+
+
+export: usado para expor funcionalidades de um
+módulo para outros.*/
