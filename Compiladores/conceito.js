0ae22807a1f86d530cb7564aa23d8e4506698bec
gustavocanepa10 <gustavociscotto@gmail.com>
2024-12-20 13:21:24 -0300

    compiladores finalizado

--- Compiladores/conceito.js
+++ Compiladores/conceito.js
@@ -2,4 +2,6 @@
 em uma versão especifica do Javascript, permitindo
 usar recursos mais recentes da linguagem mesmo em ambientes
 que não oferecem suporte a essas funcionalidades
-(por exemplo, em navegadores mais antigos).*/
+(por exemplo, em navegadores mais antigos).
+*/
+
